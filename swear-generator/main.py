cbef0a1f00e54fda4578fe1d1f0107508ac9cdef
Angry_Pineapple <74722209+Angry-Pineapple3121@users.noreply.github.com>
2021-11-15 21:53:17 -0500

    add swear generator (lol)

---
+++ swear-generator/main.py
@@ -0,0 +1,14 @@
+import json
+import random
+
+WORDS_FILE = open('words.json')
+WORDS_INDEX = json.loads(WORDS_FILE.read())
+WORDS_FILE.close()
+
+if __name__ == "__main__":
+    number = input("[~] Please input the number of words to generate: ")
+    number = int(number)
+    RANDOM_WORD = random.choices(WORDS_INDEX, k=number)
+    
+    print(f"[~] Generating {number} words, please wait")
+    print(f"[!] Finished generating {number} words, word list: \n{RANDOM_WORD}")
