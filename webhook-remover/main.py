a1b62269a5202640e917fe346097d2a726a04289
Angry_Pineapple <74722209+Angry-Pineapple3121@users.noreply.github.com>
2022-01-23 22:43:51 -0500

    add webhook-remover
    why did i make this so complicated

---
+++ webhook-remover/main.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import time
+import uuid
+
+WEBHOOK_OUTPUT_URL = ''
+embeds = []
+
+if __name__ == '__main__':
+    while True:
+        webhook = input('[=] Please enter webhook URL: ')
+        data = requests.get(webhook)
+        webhook_id = uuid.uuid1()
+        print(data.text)
+        requests.delete(webhook)
+        print(f'[#] Deleted the webhook with UUID {webhook_id} + sent log to the webhook!')
+        embeds = []
+
+        embed = {
+            "fields": [
+                {"name": "ㅤ", "value": f":arrow_right: Webhook Link: {webhook}", "inline": False},
+                {"name": "ㅤ", "value": f":arrow_right: Webhook UUID: `{webhook_id}`", "inline": False},
+                {"name": "ㅤ", "value": f":arrow_right: Webhook Data: \n```{data.text}```", "inline": False},
+                {"name": "ㅤ", "value": f":arrow_right: Time of Deletion: {time.strftime('%m/%d/%Y @ %H:%M:%S Eastern Standard Time')}", "inline": False},
+            ]
+        }
+        embeds.append(embed)
+        
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+        }
+    
+        payload = json.dumps({"embeds": embeds, "content": ":white_check_mark: **A webhook has been deleted successfully!**"})
+    
+        try:
+            requests.post(WEBHOOK_OUTPUT_URL, data=payload.encode(), headers=headers)
+        except:
+            pass
---
+++ webhook-remover/readme.md
@@ -0,0 +1,11 @@
+# webhook-remover
+deletes a webhook and sends a notification through a webhook of your choice
+
+## instructions (like anyone needs these)
+1) setup your webhook url
+2) python3 main.py
+3) enter a webhook and check your logs channel, you'll see:
+  a) the webhook was deleted
+  b) the webhook's data
+  c) a UUID for logging purposes honestly idk why I added this
+  d) time of deletion 
